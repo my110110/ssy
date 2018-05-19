f2c8a9970a8e75c6640831b3e54628d349458c80
zyh110110 <446910086@qq.com>
2018-05-19 16:04:09 +0800

    Please enter the commit message for your changes. Lines starting

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=120.79.2.167;dbname=ssy',
+    'username' => 'admin',
+    'password' => 'Zyh110110',
+    'charset' => 'utf8',
+];
