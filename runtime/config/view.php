5fc8938c657d5c06787e5c357470f6ebb4ddc176
my110110 <446910086@qq.com>
2018-05-21 20:53:14 +0800

    update ignore

--- runtime/config/view.php
+++
@@ -1,8 +0,0 @@
-<?php 
- //please do not modify this file, this file is built by app\modules\backend\models\baseConfig.php 
- return array (
-  'params' => 
-  array (
-    'themeColor' => 'red',
-  ),
-);
