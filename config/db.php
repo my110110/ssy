668897f533c7c72900455751c5c9aa8fa9c6b27d
zyh110110 <446910086@qq.com>
2018-12-12 15:27:41 +0800

    12

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=47.104.213.83;dbname=ssy',
     'username' => 'admin',
-    'password' => 'Zyh123456',
+    'password' => 'Zyh110110',
     'charset' => 'utf8',
 ];
 
